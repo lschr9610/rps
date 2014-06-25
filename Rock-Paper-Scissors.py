e082974a3d8850999607e4164c957ef60483e7b0
unknown <lschr9610@SCI-257LB-6Y9J.ad.uauth.net>
2014-06-25 19:23:02 -0400

    Added choice excitement.

--- Rock-Paper-Scissors.py
+++ Rock-Paper-Scissors.py
@@ -58,7 +58,9 @@ player2_name = raw_input("Enter player2 name: ")
 counter = 0
 while counter != rounds:
     choice1 = choice()
+    print("{} chooses: {}!!!".format(player1_name, choice1))
     choice2 = choice()
+    print("{} chooses: {}!!!".format(player2_name, choice2))
     compare(choice1, choice2)
     counter += 1
 print("{} and {} thanks for playing!".format(player1_name, player2_name))
